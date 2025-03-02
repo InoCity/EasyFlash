6d8f4317e1d5c12ea0c0197f29a7d23cacb2d348
𝓘𝓷𝓸 <162055967+InoCity@users.noreply.github.com>
2025-03-02 15:55:14 -0300

    Fix stock not booting

--- EasyFlash.bat
+++ EasyFlash.bat
@@ -158,36 +158,38 @@ REM setlocal
 	call :animated_string "Initializing Flash" 13 13 10
 	timeout /t 2 /NOBREAK >nul
 	mode 105,33
-fastboot getvar max-sparse-size 
-fastboot oem fb_mode_set 
-fastboot flash partition gpt.bin 
-fastboot flash bootloader bootloader.img 
-fastboot flash vbmeta vbmeta.img 
-fastboot flash vbmeta_system vbmeta_system.img 
-fastboot flash radio radio.img 
-fastboot flash bluetooth BTFM.bin 
-fastboot flash dsp dspso.bin 
-fastboot flash logo logo.bin 
-fastboot flash boot boot.img 
-fastboot flash vendor_boot vendor_boot.img 
-fastboot flash dtbo dtbo.img 
-fastboot flash super super.img_sparsechunk.0 
-fastboot flash super super.img_sparsechunk.1 
-fastboot flash super super.img_sparsechunk.2 
-fastboot flash super super.img_sparsechunk.3 
-fastboot flash super super.img_sparsechunk.4 
-fastboot flash super super.img_sparsechunk.5 
-fastboot flash super super.img_sparsechunk.6 
-fastboot flash super super.img_sparsechunk.7 
-fastboot flash super super.img_sparsechunk.8 
-fastboot flash super super.img_sparsechunk.9 
-fastboot erase debug_token 
-fastboot erase carrier 
-fastboot erase userdata 
-fastboot erase metadata 
-fastboot erase ddr 
-fastboot flash bluetooth BTFM.bin 
-fastboot oem fb_mode_clear 
+fastboot getvar max-sparse-size
+fastboot oem fb_mode_set
+fastboot flash partition gpt.bin
+fastboot flash bootloader bootloader.img
+fastboot flash vbmeta vbmeta.img
+fastboot flash vbmeta_system vbmeta_system.img
+fastboot flash radio radio.img
+fastboot flash bluetooth BTFM.bin
+fastboot flash dsp dspso.bin
+fastboot flash logo logo.bin
+fastboot flash boot boot.img
+fastboot flash vendor_boot vendor_boot.img
+fastboot flash dtbo dtbo.img
+fastboot flash super super.img_sparsechunk.0
+fastboot flash super super.img_sparsechunk.1
+fastboot flash super super.img_sparsechunk.2
+fastboot flash super super.img_sparsechunk.3
+fastboot flash super super.img_sparsechunk.4
+fastboot flash super super.img_sparsechunk.5
+fastboot flash super super.img_sparsechunk.6
+fastboot flash super super.img_sparsechunk.7
+fastboot flash super super.img_sparsechunk.8
+fastboot flash super super.img_sparsechunk.9
+fastboot flash super super.img_sparsechunk.10
+fastboot erase debug_token
+fastboot erase carrier
+fastboot erase userdata
+fastboot erase metadata
+fastboot erase ddr
+fastboot oem fb_mode_clear
+fastboot oem config unset console
+fastboot oem config unset cmdl
 cls
 mode 30,10
 echo %q% Stockrom flashed sucessfully!
